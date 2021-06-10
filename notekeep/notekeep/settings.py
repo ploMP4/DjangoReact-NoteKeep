113e26df9884dc17c67b7739b36e3c70ad78d34b
ploMP4 <kostasarto2@gmail.com>
2021-06-10 19:11:27 +0300

    -

--- notekeep/notekeep/settings.py
+++ notekeep/notekeep/settings.py
@@ -11,16 +11,19 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q=ras^=#la=+c(s+!h3b5$955qho-l(m4+2^%m!4p+fdp%=919'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
